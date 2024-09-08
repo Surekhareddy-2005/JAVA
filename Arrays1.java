237ad05288f7d1df78e6959d8257e97c557f3a99
SUREKHA REDDY GUDIMETLA <23A91A0519@aec.edu.in>
2024-09-08 13:19:16 +0530

    Update Arrays1.java

--- Arrays1.java
+++ Arrays1.java
@@ -11,7 +11,12 @@
 
 
 import java.util.Scanner;
+class student{
+    int marks;
+    int roll;
+    String name;
 
+}
 public class Arrays1 {
    public static void main(String[] args){
 
@@ -44,6 +49,37 @@ public class Arrays1 {
         System.out.println(k+" ");
      }
      System.out.println();
+
+    // creating an array from your own type
+       student s1= new student();
+        s1.roll=19;
+        s1.name="surekha";
+        s1.marks=9;
+        student s2= new student();
+        s2.roll=29;
+        s2.name="geetha";
+        s2.marks=8;
+        student s3=new student();
+        s3.roll=28;
+        s3.name="hari";
+        s3.marks=10;
+
+        student students[]= new student[3];
+        students[0]=s1;
+        students[1]=s2;
+        students[2]=s3;
+
+        for(int i=0;i<students.length;i++)
+        {
+            System.out.println(students[i].name +":"+students[i].roll+":"+students[i].marks);
+        }
+        System.out.println();
+
+        for(student stud: students)
+        {
+            System.out.println(stud.name +":"+stud.roll+":"+stud.marks);
+        }
+
 
 }
 }
